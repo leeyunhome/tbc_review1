1899c2421c588f0e2b6a4f5c12adc90f8debbbfd
leeyunhome <leeyunhome@daum.net>
2020-05-31 21:20:44 +0900

    15.11 비트필드의 패딩(2)

--- Chapter15/15.11 비트필드의 패딩/main.c
+++ Chapter15/15.11 비트필드의 패딩/main.c
@@ -20,10 +20,9 @@ void print_binary(char* data, int bytes)    // Note: extended for any sizes
 
 int main()
 {
-    bool a = true;
     struct {
         bool option1 : 1;
-        bool         : 0;
+        //bool         : 0;
         bool option2 : 1;
         //unsigned long long option3 : 16;
     } bbf;
@@ -42,6 +41,18 @@ int main()
         unsigned short option2 : 1;
         //unsigned short : 0;
         unsigned short option3 : 1;
-    };
+    } usbf;
+
+    printf("%zu bytes\n", sizeof(usbf));
+
+    struct {
+        unsigned int option1 : 1;
+        //unsigned int : 0;
+        unsigned int option2 : 1;
+        //bool option3 : 1;
+    } uibf;
+
+    printf("%zu bytes\n", sizeof(uibf));
+
     return 0;
 }
